fd280f54f6989fdce30dd76df7d8a5cfb24efdfb
prabhuvel <82199217+prabhuvel@users.noreply.github.com>
2021-06-19 12:54:24 +0530

    Dashboard Screen setup Added

---
+++ kathai_neram/lib/Web/DashboardPage/DashBoardScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashBoardScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new DashBoardScreenState();
+  }
+}
+class DashBoardScreenState extends State<DashBoardScreen>{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold();
+  }
+}
