d8a230caf11e5abe6014a0012588fc618d67e9ef
vickyadhi <vickyadhi011@gmail.com>
2021-06-19 03:03:52 -0700

    Firestore
    Initial source code added

--- kathai_neram/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ kathai_neram/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -13,5 +13,10 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new com.ryanheise.audio_session.AudioSessionPlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin());
+    flutterEngine.getPlugins().add(new com.ryanheise.just_audio.JustAudioPlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
   }
 }
